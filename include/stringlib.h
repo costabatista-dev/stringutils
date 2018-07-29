2ebb4b23456a77f1b75dde25b3b39720c9ce5a41
Paulo Batista <pauloc@linuxmail.org>
2018-07-29 11:38:11 -0300

    a function to verify if two strings have the same content. string_cmp was developed. closes #1

---
+++ include/stringlib.h
@@ -0,0 +1,14 @@
+#ifndef _STRINGLIB1_
+#define _STRINGLIB1_
+/*========================================================================
+======================== function: string_cmp ============================
+@param_0: The first string to be compared  
+@param_1: The second string to be compared
+@return: It returns 0 if the two given string are equal
+@return: It returns 1 if the first string is higher than the second one
+@return: It returns -1 if the second string is lesser than the second one
+==========================================================================*/
+int string_cmp(char *, char *);
+
+
+#endif
---
+++ lib/stringlib.c
@@ -0,0 +1,13 @@
+#include "../include/stringlib.h"
+
+
+int string_cmp(char *str1, char *str2) {
+    int i = 0;
+    while(str1[i] != '\0') {
+        if (str1[i] != str2[i]) {
+            return str1[i] > str2[i] ? 1 : -1;
+        }
+        else i++;
+    }
+    return 0;     
+}
