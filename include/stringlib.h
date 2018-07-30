02f01f3bb3bd9aeae646c361c3028c10d4e0a3d4
Paulo Batista <pauloc@linuxmail.org>
2018-07-30 13:15:47 -0300

    fixed param names in documentation header file

--- include/stringlib.h
+++ include/stringlib.h
@@ -4,8 +4,8 @@
 /*========================================================================
 ======================== function: string_cmp ============================
 A function to compare if two strings have the same content (are equal)
-@param_0: The first string to be compared  
-@param_1: The second string to be compared
+@param str1: The first string to be compared  
+@param str2: The second string to be compared
 @return: It returns 0 if the two given string are equal
 @return: It returns 1 if the first string is higher than the second one
 @return: It returns -1 if the second string is lesser than the second one
@@ -15,7 +15,7 @@ int string_cmp(char *str1, char *str2);
 /*========================================================================
 ======================== function: string_length =========================
 A function to get the size of a string
-@param_0: The inputted string to be measured
+@param str: The inputted string to be measured
 @return: It returns the length of the inputted string (int)
 ==========================================================================*/
 int string_length(char *str);
@@ -23,16 +23,16 @@ int string_length(char *str);
 /*========================================================================
 ======================== function: string_copy ===========================
 A function to copy a string from a source to a destiny
-@param_0: The destiny string that will be receive content from another one
-@param_1: The string source that will be copied
+@param dest: The destiny string that will be receive content from another one
+@param src: The string source that will be copied
 ==========================================================================*/
 void string_copy(char *dest, char *src);
 
 /*========================================================================
 ===================== function: string_concat ============================
 A function to concatenate two given strings
-@param_0: First string, it'll be putted at the begining of the resultant string
-@param_1: Second string, it'll be putted at the end of the resultant string
+@param str1: First string, it'll be putted at the begining of the resultant string
+@param str2: Second string, it'll be putted at the end of the resultant string
 @return: A string that's the result of concatenation of second string
 at the end of the first string
 ==========================================================================*/
@@ -42,7 +42,8 @@ char* string_nconcat(char *dest, char *src, int n);
 /*========================================================================
 ===================== function: string_init_cut ==========================
 A function to cut a string from the begining until a given index (inclusive)
-@param_0: A string that will be cutted from the begining
+@param str: A string that will be cutted from the begining
+@para index: a index to cut the inputted string
 ==========================================================================*/
 void string_init_cut(char *str, int index);
 
@@ -50,9 +51,9 @@ void string_init_cut(char *str, int index);
 ===================== function: substring_at_be ==========================
 A function to get a substring through a begin index and a end index, given
 a string
-@param_0: A source string
-@param_1: An int that indicates the inital index of the substring (inclusive)
-@param_2: An int that indicates the end index of the substring (inclusive)
+@param str: A source string
+@param begin: An int that indicates the inital index of the substring (inclusive)
+@param end: An int that indicates the end index of the substring (inclusive)
 @return: It returns a substring that correspond the given indexes
 ==========================================================================*/
 char* substring_at_be(char *str, int begin, int end);
